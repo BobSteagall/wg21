85410ade5cc88dff6ae50acb37046d6913385b81
Bob Steagall <bob.steagall@comcast.net>
2019-02-19 23:30:41 -0500

    Add customization examples

--- linear_algebra/code/test_new_arithmetic.hpp
+++ linear_algebra/code/test_new_arithmetic.hpp
@@ -22,7 +22,42 @@ struct matrix_addition_traits<matrix<fs_matrix_engine_tst<T1, R, C>, OTR>,
                             PrintOperandTypes<result_type>("addition_traits_TST", m1, m2);
                             return result_type();
                         }
+};
+/*
+template<class T1, class T2>
+struct matrix_multiplication_traits<matrix<fs_matrix_engine<T1, 4, 4>, default_matrix_operator_traits>, 
+                                    column_vector<fs_matrix_engine<T2, 4, 1>, default_matrix_operator_traits>, 
+                                    default_matrix_operator_traits>
+{
+    using engine_type = matrix_multiplication_engine_t<fs_matrix_engine<T1, 4, 4>, 
+                                                       fs_matrix_engine<T2, 4, 1>>;
 
+    using result_type = column_vector<engine_type, default_matrix_operator_traits>;
+
+    static result_type  multiply(matrix<fs_matrix_engine<T1, 4, 4>, default_matrix_operator_traits> const& m1, 
+                                 column_vector<fs_matrix_engine<T2, 4, 1>, default_matrix_operator_traits> const& cv2)
+                        {
+                            PrintOperandTypes<result_type>("multiplication_traits_4x4*4x1", m1, cv2);
+                            return result_type();
+                        }
+};
+*/
+template<>
+struct matrix_multiplication_traits<fs_matrix<float, 4, 4>, 
+                                    fs_column_vector<float, 4>, 
+                                    default_matrix_operator_traits>
+{
+    using engine_type = matrix_multiplication_engine_t<fs_matrix<float, 4, 4>::engine_type, 
+                                                       fs_column_vector<float, 4>::engine_type>;
+
+    using result_type = column_vector<engine_type, default_matrix_operator_traits>;
+
+    static result_type  multiply(fs_matrix<float, 4, 4> const& m1, 
+                                 fs_column_vector<float, 4> const& cv2)
+                        {
+                            PrintOperandTypes<result_type>("multiplication_traits_4x4*4x1", m1, cv2);
+                            return result_type();
+                        }
 };
 }
 #endif  //- TEST_NEW_ARITHMETIC_HPP_DEFINED
