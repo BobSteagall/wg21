448ecfd29d3d495fa13eb24be55421b3656f36fd
Bob Steagall <bob.steagall@gliacelltechnologies.com>
2019-04-04 13:00:14 -0400

    Updating operator traits - wip

--- linear_algebra/code/include/linear_algebra/helper_traits.hpp
+++ linear_algebra/code/include/linear_algebra/helper_traits.hpp
@@ -21,14 +21,14 @@ using has_rx_2 = has_resize_member<dr_matrix_engine<CT>, void, ST, ST>;
 template<class CT, class ST>
 using has_rx_4 = has_resize_member<dr_matrix_engine<CT>, void, ST, ST, ST, ST>;
 
-/*
-template<typename CT, typename ST, typename = void>
+
+template<typename CT, typename = void>
 struct has_resize_x : std::false_type {};
 
-template<typename CT, typename ST>
-struct has_resize_x<CT, std::void_t<decltype(detection_helper<void (CT::*)(ST,ST), &CT::resize>())>>
+template<typename CT>
+struct has_resize_x<CT, std::void_t<decltype(detection_helper<void (CT::*)(typename CT::size_type, typename CT::size_type), &CT::resize>())>>
 :   std::true_type {};
-*/
+
 
 template<class RT, class O1>
 void
