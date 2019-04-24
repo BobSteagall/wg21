75159cf6e03dc37cb6f16e4999dfe3ba63d89383
Guy Davidson <guy@hatcat.com>
2019-04-24 18:52:13 +0100

    matrix negation

--- linear_algebra/code/include/linear_algebra/negation_traits_impl.hpp
+++ linear_algebra/code/include/linear_algebra/negation_traits_impl.hpp
@@ -27,7 +27,7 @@ matrix_negation_traits<OT, vector<ET1, OT1>>::negate(vector<ET1, OT1> const& v1)
 	}
 
 	transform(v1.data(), v1.data() + v1.elements(), vr.data(),
-		[](auto val) {return val * -1; });
+		[](auto val) {return -val; });
 
 	return vr;
 }
@@ -39,7 +39,26 @@ inline auto
 matrix_negation_traits<OT, matrix<ET1, OT1>>::negate(matrix<ET1, OT1> const& m1) -> result_type
 {
     PrintOperandTypes<result_type>("negation_traits", m1);
-    return result_type();
+
+	result_type		mr;
+	const auto		rows = m1.rows();
+	const auto		columns = m1.columns();
+
+	if constexpr (result_requires_resize(mr))
+	{
+		mr.resize(rows, columns);
+		auto data = mr.data();
+		for (auto i = 0; i < columns; ++i)
+			for (auto j = 0; j < rows; ++j)
+				*data++ = -m1(i, j);		// Safe because the resize means that mr capacity = size for rows and columns.
+	}
+	else
+	{
+		transform(m1.data(), m1.data() + (rows * columns), mr.data(),
+			[](auto val) {return -val; });
+	}
+
+	return mr;
 }
 
 }       //- STD_LA namespace
