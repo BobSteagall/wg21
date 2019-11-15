b10df59a3ae3b1f3f84dc0adb5d31438297669e5
Antony Peacock <ant.peacock@gmail.com>
2019-11-15 12:58:05 +0000

    Add Conan Packaging for linear algebra

---
+++ .conan/test_package/conanfile.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+from conans import ConanFile, CMake, tools
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if tools.cross_building(self.settings):
+            return
+        bin_path = os.path.join("bin", "test_linear_algebra")
+        self.run(bin_path, run_environment=True)
