5047d47dc057214a6f8d8bad2064e1f2d63f3e2e
Antony Peacock <ant.peacock@gmail.com>
2020-01-24 16:50:42 +0000

    Remove GTests from Conan Test Package dependencies, and WG21 Linear Algebra expot target

---
+++ .conan/test_package/test_package.cpp
@@ -0,0 +1,11 @@
+#include <linear_algebra.hpp>
+
+using namespace STD_LA;
+
+int main()
+{
+    vector<fs_vector_engine<double, 4>> vector;
+    matrix<fs_matrix_engine<double, 4, 4>> matrix;
+    vector * matrix;
+    return EXIT_SUCCESS;
+}
