d1a583f825d63465caf4737689056ee872a231ea
Bob Steagall <bob.steagall@comcast.net>
2020-10-11 19:07:55 -0400

    Some progress on new addition engine traits

--- tests/test_op_add.cpp
+++ tests/test_op_add.cpp
@@ -2,8 +2,60 @@
 #include "test_common.hpp"
 
 using namespace STD_LA;
+//using namespace MDSPAN_NS;
+
+template<class OTR, class ET1, class ET2>
+struct addition_engine_traits2
+{
+  private:
+    static_assert(STD_LA::detail::similar_engines<ET1, ET2>);
+
+    using element_type_1 = typename ET1::element_type;
+    using element_type_2 = typename ET2::element_type;
+    using element_traits = get_addition_element_traits_t<OTR, element_type_1, element_type_2>;
+
+    template<bool is_matrix> struct mat_t;
+
+    template<>
+    struct mat_t<true>
+    {
+        //- Get the extents for each engine
+        //
+        static constexpr ptrdiff_t  R1 = STD_LA::detail::extent_helper<ET1>::rows();
+        static constexpr ptrdiff_t  C1 = STD_LA::detail::extent_helper<ET1>::columns();
+        static constexpr ptrdiff_t  R2 = STD_LA::detail::extent_helper<ET2>::rows();
+        static constexpr ptrdiff_t  C3 = STD_LA::detail::extent_helper<ET2>::columns();
+
+        //- Determine there are any dynamic row or column extents.
+        //
+        static constexpr bool   dyn_rows = ((R1 == dynamic_extent) || (R2 == dynamic_extent));
+        static constexpr bool   dyn_cols = ((C1 == dynamic_extent) || (C2 == dynamic_extent));
+
+        //- Validate the extents.
+        //
+        static_assert((R1 == R2 || dyn_rows), "mis-matched/invalid number of rows for addition");
+        static_assert((C1 == C2 || dyn_cols), "mis-matched/invalid number of columns for addition");
+
+        //- Decide on the new extents.
+        //
+        static constexpr ptrdiff_t  RR = (dyn_rows) ? dynamic_extent : R1;
+        static constexpr ptrdiff_t  CR = (dyn_cols) ? dynamic_extent : C1;
+    };
+
+    template<>
+    struct mat_t<false>
+    {
+    };
+
+  public:
+    using element_type = typename element_traits::element_type;
+//    using engine_type  = conditional_t<readable_matrix_engine<ET1>,
+//                                       dynamic_matrix_engine<element_type>,
+//                                       dynamic_vector_engine<element_type>>;
+};
+
+
 using namespace STD_LA::detail;
-using namespace MDSPAN_NS;
 
 using fmf_33 = fixed_size_matrix<float, 3, 3>;
 using fmf_44 = fixed_size_matrix<float, 4, 4>;
@@ -40,8 +92,6 @@ get_row_extent()
 {
     return get_extent_helper([]{ return static_cast<ptrdiff_t>(ET().rows()); });
 }
-
-
 
 TEST(Add, Constexpr)
 {
